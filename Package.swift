92940963ce558012b5c0fc16ffd0789e6bd46399
LugaWang <luga.wang@applovin.com>
2026-06-08 11:18:56 -0700

    release/7.7.4.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", from: "13.0.0"),
-        .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", exact: "7.7.3")
+        .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", exact: "7.7.4")
     ],
     targets: [
         .target(
@@ -28,8 +28,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AppLovinMediationVungleAdapter",
-            url: "https://artifacts.applovin.com/ios/com/applovin/mediation/vungle-adapter/AppLovinMediationVungleAdapter-7.7.3.0.zip",
-            checksum: "61e3b0715a9feca76078d7316548ecb959fe0fa029adb86db98ae82483316830"
+            url: "https://artifacts.applovin.com/ios/com/applovin/mediation/vungle-adapter/AppLovinMediationVungleAdapter-7.7.4.0.zip",
+            checksum: "b75d6f0c68d97e62e9607156e4b9e9f00e65c35ebc1a70fabe13f426512e6c75"
         )
     ]
 )
