8b0aa27a6f8b4dc1e7c3f63c6683dbefea97d5cb
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2026-07-17 11:38:12 -0700

    7.7.5.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", from: "13.0.0"),
-        .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", exact: "7.7.4")
+        .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", exact: "7.7.5")
     ],
     targets: [
         .target(
@@ -28,8 +28,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AppLovinMediationVungleAdapter",
-            url: "https://artifacts.applovin.com/ios/com/applovin/mediation/vungle-adapter/AppLovinMediationVungleAdapter-7.7.4.0.zip",
-            checksum: "b75d6f0c68d97e62e9607156e4b9e9f00e65c35ebc1a70fabe13f426512e6c75"
+            url: "https://artifacts.applovin.com/ios/com/applovin/mediation/vungle-adapter/AppLovinMediationVungleAdapter-7.7.5.0.zip",
+            checksum: "e07db7802ea601a64fde3b1d776b74c74cb42d843ce0adaace0e1bd2dc980ea8"
         )
     ]
 )
